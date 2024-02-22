f0479ed184855d99f899b65da2981ef005aa8f5a
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-02-22 16:12:46 +0000

    Created new app about, setup views, URLs

--- about/views.py
+++ about/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse ("Hello, world!")
-
+def about_me(request):
+    return HttpResponse("This would be the about page")
