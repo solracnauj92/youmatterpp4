a1aebaba1d086f82e3bf6fab71b5ddec6b494d22
Juan Carlos Diaz Lara <juancarlosdiazlara@gmail.com>
2024-03-01 01:57:45 +0000

    Add tag model and fix error by tesing

--- hello_world/admin.py
+++ hello_world/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Comment, Tag
 from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
@@ -18,3 +18,8 @@ class PostAdmin(SummernoteModelAdmin):
 
 # Register your models here.
 admin.site.register(Comment)
+
+@admin.register(Tag)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
